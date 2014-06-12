9960faf1f5eb5360f29dbe442b9db5d7b10b97b2
Jānis <janis.indans@gmail.com>
2014-06-12 13:41:34 +0300

    Fixed layout style and buttons

--- app/src/main/java/com/callcone/app/MainActivity.java
+++ app/src/main/java/com/callcone/app/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.calculator_three);
+        setContentView(R.layout.calculator_four);
 
 
     }
